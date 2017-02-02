e106d68c19b513723d6658280ed8898eb6d6ee96
3marproof <omar0star@yahoo.com>
2017-02-02 19:41:34 +0200

    Add files via upload

---
+++ teleport/teleport.cs
@@ -0,0 +1,27 @@
+﻿using GTANetworkServer;
+using GTANetworkShared;
+
+public class Teleport : Script
+{
+
+    [Command("tp")]
+    public void tptoplayer(Client sender, Client player)
+    {
+        if (player.isInVehicle)
+        {
+
+            API.sendChatMessageToPlayer(sender, "Teleporting to " + player.name);
+            API.setPlayerIntoVehicle(sender, player.vehicle.handle, -2);
+
+        }
+        else
+        {
+            API.sendChatMessageToPlayer(sender, "Teleporting to " + player.name);
+            API.setEntityPosition(sender.handle, API.getEntityPosition(player.handle));
+        }
+
+    }
+}
+
+
+
